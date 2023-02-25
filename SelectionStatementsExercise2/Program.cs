6f9a5c084434eb3ad20ad29bf69b5e6d67e2cf12
jaunitageorge1 <jaunitageorge@gmail.com>
2023-02-24 20:57:10 -0500

    First commit in a while.

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("You -- yes, I'm looking at you. What's my favorite school subject?");
+
+            var userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                    case "history": Console.WriteLine("Don't know much about history.");
+                      break; 
+                    case "biology": Console.WriteLine("Don't know much biology");
+                      break;
+                    case "science": Console.WriteLine("Don't know much about the science books");
+                      break;
+                    case "french": Console.WriteLine("Don't know much about the French I took");
+                      break;
+                    case "c#": Console.WriteLine("You guessed it right! Whoo!!");
+                      break;
+                    default: Console.WriteLine("Really? Never heard that one before.");
+                      break;
+            }
+
         }
     }
 }
+
